73556f25a78b018464a7e0fff48316bc35992111
Anjali-st <anjali90908080@gmail.com>
2023-08-15 18:43:24 +0530

    initial commit

---
+++ Teacher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConApp_Inheritance
+{
+
+    public class Teacher
+    {
+        internal object whichClass;
+        private string name;
+        private string address;
+        private string contactAddress;
+        private DateTime dateOfJoining;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string ContactAddress
+        {
+            get { return contactAddress; }
+            set { contactAddress = value; }
+        }
+
+        public DateTime DateOfJoining
+        {
+            get { return dateOfJoining; }
+            set { dateOfJoining = value; }
+        }
+    }
+}
+
